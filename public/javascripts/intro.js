c636643400dcb99b51f606eb8b7ec0665f279a95
Avika Narula <an1520@nyu.edu>
2016-09-26 01:06:00 -0400

    revamp

--- public/javascripts/intro.js
+++ public/javascripts/intro.js
@@ -1,40 +1,17 @@
+    
     var w = 700;
     var h = 200;
     var numelements = 700;
     var angle = 0.0;
     var angleVel = 0.05;
     var startAngle = 0;
-
-      /*
-        some stuff to track your mouse movements
-      */
-    var interval = 500;  //update display every 500ms
-    var lastmousex = -1;
-    var lastmousey = -1; 
-    var lastmousetime; 
-    var mousetravel = 0; 
-
-    var waveScale = d3.scale.linear().domain([0,10]).range([0.030,0.080]).clamp([true]);
-
-    $('html').mousemove(function(e) {
-         var mousex = e.pageX;
-         var mousey = e.pageY;
-         if (lastmousex > -1)
-             mousetravel = Math.max( Math.abs(mousex-lastmousex), Math.abs(mousey-lastmousey) );
-         lastmousex = mousex;
-         lastmousey = mousey;
-    });
-
-
-    //update mousetravel if mouse stops
+    var nodeSize = 30; 
 
     noise.seed(Math.random());
 
-    
     var arrowSvg = d3.select("#arrow-svg").append('svg')
           .attr('width', 60)
           .attr('height', 45);
-
 
     arrowSvg.append("path")
             .attr('d', "M12 10 L30 32 L50 10")
@@ -48,54 +25,63 @@
             .attr('stroke-width', "2px")
             .attr('stroke', '#219bb5');
 
-     
-      //create an svg
+    var arrowSvg2 = d3.select("#arrow-svg-2").append('svg')
+          .attr('width', 60)
+          .attr('height', 45);
+
+    arrowSvg2.append("path")
+            .attr('d', "M12 10 L30 32 L50 10")
+            .attr("fill", "none")
+            .attr('stroke-width', '2px')
+            .attr('stroke', '#219bb5');
+
+    arrowSvg2.append("path")
+            .attr("d", "M12 20 L30 42 L50 20")
+            .attr("fill", "none")
+            .attr('stroke-width', "2px")
+            .attr('stroke', '#219bb5');
+
+    //create an svg
     var svg = d3.select("#intro-svg").append('svg');
-      //set svg attributes
+ 
     svg.attr('width', w)
        .attr('height', h);
 
-    var x_pos = 0;
-    var y_pos = 0;
     var pts = [];
     var yScale = d3.scale.linear().domain([-1, 1]).range([(h/2) + 50, (h/2) - 50]);
 
     for(var i = 0; i<numelements; i+=10)
       {
         var x_position = i;
-        var y_position = yScale(Math.pow(Math.sin(angle),6));
         var y_noise = yScale(noise.perlin2(angle,angle));
         pts.push({x: x_position, y:y_noise});
         angle+=angleVel;
       }
 
-      //line function
+    //line, returns a function
     var line = d3.svg.line()
           .x(function(d) { return d.x; })  //use to set x of point
           .y(function(d) { return d.y; }) // set y of point
           .interpolate("basis");
 
-      // create a parent element the scene graph
     var group = svg.append('g').attr('transform', 'translate(30,10)');
 
-      // append a <defs> element
+    // append a <defs> element
     var defs = group.append('defs');
 
-      //set path with points
+    //set path with points
+    //shapes inside def elements are not displayed, until they are referenced
+    //path takes a line, which is a function 
     var path = defs.append('path')
             .attr('id', 'our_path')  
-            //enter a straight line first    
             .attr('d', line(pts))
             .attr('fill', 'none')
             .attr('stroke', '#219bb5')
-            .style('stroke-width', '1.5px');
+            .style('stroke-width', '2.5px');
 
-
-    // make the line the text is on visible by actually drawing it
-    // textPath doesn't draw the line, it makes sense
+    //create a use group to use defs element 
     var path_use = group.append("use")
           .attr("xlink:href", "#our_path");
-
 
     var txt = group.append('text') 
             .attr("dy", -12)
@@ -104,7 +90,6 @@
             .style('font-size', '50px')
             .style('font-family', 'Kranky')
             .style('opacity', '0.1');
-
 
     var textPath = txt.append('textPath') // create a textPath element with a fill an actual text
             .attr('startOffset', '40%')        //to text append textpath
@@ -119,25 +104,25 @@
           .ease('linear')
           .style('opacity', '1');
 
- 
-     wave();
+    wave();
 
-      function wave()
+    function wave()
+    {
+      startAngle+= 0.05;
+      angle = startAngle;
+      pts = [];
+      for(var i = 0; i<numelements; i+=10)
       {
-        startAngle+= waveScale(mousetravel);
-        angle = startAngle;
-        pts = [];
-        for(var i = 0; i<numelements; i+=10)
-        {
-          var x_position = i;
-          var y_position = yScale(Math.pow(Math.sin(angle),2));
-          var y_noise = yScale(noise.perlin2(angle,angle));
-          pts.push({x: x_position, y:y_noise});
-          angle+=angleVel;
-        }
-        //get a smooth transition to the new line
-        //recursive
-        path.transition().attr('d', line(pts))
-            .ease("linear")
-            .each('end', wave);
+        var x_position = i;
+        var y_noise = yScale(noise.perlin2(angle,angle));
+        pts.push({x: x_position, y:y_noise});
+        angle+=angleVel;
       }
+      //get a smooth transition to the new line
+      //recursive
+      path.transition().attr('d', line(pts))
+          .ease("linear")
+          .each('end', wave);
+    }
+    
+   

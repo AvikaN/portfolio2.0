23ddd58068fe48a471930850e68c5203515057b7
Avika Narula <an1520@nyu.edu>
2016-10-06 15:10:38 -0400

    d3 project

--- public/javascripts/arrow.js
+++ public/javascripts/arrow.js
@@ -1,3 +1,20 @@
+var sideArrow = d3.selectAll('.side-arrow').append('svg')
+        .attr('width', 45)
+        .attr('height', 60);
+
+    sideArrow.append('path')
+        .attr('d', "M12 10 L30 32 L50 10")
+        .attr("fill", "none")
+        .attr("stroke-width", '2px')
+        .attr("stroke", "#d94701");
+
+    sideArrow.append('path')
+        .attr("d", "M12 20 L30 42 L50 20")
+        .attr("fill", "none")
+        .attr('stroke-width', "2px")
+        .attr("stroke", "#d94701");
+
+
 var arrowSvg = d3.selectAll(".arrow-svg").append('svg')
           .attr('width', 60)
           .attr('height', 45);
